41d688575cfe3362b3d292fb6209d1cc1ea62913
Henrique <fischxr@hotmail.com>
2023-06-17 12:33:32 -0300

    dev

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 import 'dotenv/config'
 import * as config from '../config/database'
 
-// const sequelize = new Sequelize(config);
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(config);
+// const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 export default sequelize;
