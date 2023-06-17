f5ae40462fbc743e6deb1de4b71af4764c8d48c8
Henrique <fischxr@hotmail.com>
2023-06-17 14:13:24 -0300

    dev

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import App from './app';
 import 'express-async-errors';
 import 'dotenv/config'
 
-// const PORT = process.env.MYSQLPORT;
-new App().start(Number(3000));
+const PORT = process.env.APP_PORT;
+new App().start(Number(PORT));
