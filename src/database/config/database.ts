15f67ce1e9671d12c6f423fc44bb882927a1af6b
fischer8 <fischxr@hotmail.com>
2023-07-17 16:08:17 -0300

    test3

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -10,7 +10,7 @@ const config: Options = {
   dialect: 'postgres',
   dialectOptions: {
     timezone: 'Z',
-    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+    connectionString: process.env.POSTGRES_URL,
     ssl: {
       require: true,
       rejectUnauthorized: false,
