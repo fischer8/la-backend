bb8d87c11f21c51413f0b8881d2ea8a2ad4b19c0
fischer8 <fischxr@hotmail.com>
2023-07-17 12:40:12 -0300

    dev

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -2,11 +2,11 @@ import { Options } from 'sequelize';
 import 'dotenv/config'
 
 const config: Options = {
-  username: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  host: process.env.MYSQLHOST,
-  port: Number(process.env.MYSQLPORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   dialect: 'postgres',
   dialectOptions: {
     timezone: 'Z',
