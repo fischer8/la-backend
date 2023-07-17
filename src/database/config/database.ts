5b2a548f2e0ad58b65d87ccdc51d1638e3b170f3
fischer8 <fischxr@hotmail.com>
2023-07-17 13:03:43 -0300

    dev

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -2,15 +2,15 @@ import { Options } from 'sequelize';
 import 'dotenv/config'
 
 const config: Options = {
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
   dialect: 'postgres',
   dialectOptions: {
     timezone: 'Z',
-    connectionString: process.env.DB_URL + "?sslmode=require",
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
   },
   logging: false,
 }
