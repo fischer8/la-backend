729c792099813588d7c066855fe03bd5ca0745f2
fischer8 <fischxr@hotmail.com>
2023-07-17 13:25:11 -0300

    test

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -10,7 +10,7 @@ const config: Options = {
   dialect: 'postgres',
   dialectOptions: {
     timezone: 'Z',
-    connectionString: process.env.POSTGRES_URL + "?ssl=true",
+    connectionString: process.env.POSTGRES_URL,
   },
   logging: false,
 }
